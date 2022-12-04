6696b3d687488e2bae76b5fc2e4b79a6e53604d1
Mattias Kasemets <mattiaskasemets@gmail.com>
2022-12-04 22:57:11 +0200

    Make day4 code more efficient.

--- advent-of-code-2022/puzzle4/Advent4.cs
+++ advent-of-code-2022/puzzle4/Advent4.cs
@@ -15,54 +15,46 @@ namespace advent_of_code_2022.puzzle4
             return inputData.OpenText();
         }
 
-        public static List<Tuple<string, string>> ProcessInputFile(TextReader text)
+        public static List<Tuple<int, int, int, int>> ProcessInputFile(TextReader text)
         {
-            var results = new List<Tuple<string, string>>();
+            var results = new List<Tuple<int, int, int, int>>();
             while (text.ReadLine() is { } line)
             {
-                string firstAssignment = line.Split(',').First();
-                string secondAssignment = line.Split(',').Last();
-                results.Add(new Tuple<string, string>(firstAssignment, secondAssignment));
+                var lineSplit = line.Split(',');
+                var firstAssignmentString = lineSplit.First().Split('-');
+                var secondAssignmentString = lineSplit.Last().Split('-');
+                int firstAssignmentStartRange = int.Parse(firstAssignmentString[0]);
+                int firstAssignmentEndRange = int.Parse(firstAssignmentString[1]);
+                int secondAssignmentStartRange = int.Parse(secondAssignmentString[0]);
+                int secondAssignmentEndRange = int.Parse(secondAssignmentString[1]);
+                results.Add(new Tuple<int, int, int, int>(firstAssignmentStartRange, firstAssignmentEndRange
+                    , secondAssignmentStartRange, secondAssignmentEndRange));
             }
             return results;
         }
 
-        public static bool AssignmentFullyIncludedInOther(Tuple<string, string> input)
+        public static bool AssignmentFullyIncludedInOther(Tuple<int, int, int, int> input)
         {
-            var firstAssignmentNumbers = input.Item1.Split('-').Select(i => int.Parse(i.ToString()));
-            var secondAssignmentNumbers = input.Item2.Split('-').Select(i => int.Parse(i.ToString()));
-            var minimumFirst = firstAssignmentNumbers.Min();
-            var minimumSecond = secondAssignmentNumbers.Min();
-            var maximumFirst = firstAssignmentNumbers.Max();
-            var maximumSecond = secondAssignmentNumbers.Max();
-            return (minimumFirst <= minimumSecond
-                    && maximumFirst >= maximumSecond
-                    || minimumSecond <= minimumFirst
-                    && maximumSecond >= maximumFirst);
+            return (input.Item1 <= input.Item3
+                    && input.Item2 >= input.Item4
+                    || input.Item3 <= input.Item1
+                    && input.Item4 >= input.Item2);
         }
         
-        public static bool AssignmentOverlapsWithOther(Tuple<string, string> input)
+        public static bool AssignmentOverlapsWithOther(Tuple<int, int, int, int> input)
         {
-            var firstAssignmentNumbers = input.Item1.Split('-')
-                .Select(i => int.Parse(i.ToString()));
-            var secondAssignmentNumbers = input.Item2.Split('-')
-                .Select(i => int.Parse(i.ToString()));
-            var minimumFirst = firstAssignmentNumbers.Min();
-            var minimumSecond = secondAssignmentNumbers.Min();
-            var maximumFirst = firstAssignmentNumbers.Max();
-            var maximumSecond = secondAssignmentNumbers.Max();
-            return (minimumFirst >= minimumSecond && minimumFirst <= maximumSecond
-                || maximumFirst <= maximumSecond && maximumFirst >= minimumSecond
-                || minimumSecond >= minimumFirst && minimumSecond <= minimumFirst
-                || maximumSecond <= maximumFirst && maximumSecond >= minimumFirst);
+            return (input.Item1 >= input.Item3 && input.Item1 <= input.Item4
+                    || input.Item2 <= input.Item4 && input.Item2 >= input.Item3
+                    || input.Item3 >= input.Item1 && input.Item3 <= input.Item1
+                    || input.Item4 <= input.Item2 && input.Item4 >= input.Item1);
         }
         
-        public static int Part1Solution(IEnumerable<Tuple<string, string>> data)
+        public static int Part1Solution(IEnumerable<Tuple<int, int, int, int>> data)
         {
             return data.Count(i => AssignmentFullyIncludedInOther(i));
         }
 
-        public static int Part2Solution(IEnumerable<Tuple<string, string>> data)
+        public static int Part2Solution(IEnumerable<Tuple<int, int, int, int>> data)
         {
             return data.Count(i => AssignmentOverlapsWithOther(i));
         }
